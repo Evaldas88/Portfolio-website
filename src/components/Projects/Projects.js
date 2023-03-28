18271bdfa2d924ef760aad9a82b74554c5b7bd6e
Evaldas88 <e.skackauskas@gmail.com>
2023-03-28 10:49:00 +0300

    first commit

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Projects.module.css';
+
+const Projects = () => (
+  <div className={styles.Projects} data-testid="Projects">
+    Projects Component
+  </div>
+);
+
+Projects.propTypes = {};
+
+Projects.defaultProps = {};
+
+export default Projects;
---
+++ src/components/Skills/Skills.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Skills.module.css';
+
+const Skills = () => (
+  <div className={styles.Skills} data-testid="Skills">
+    Skills Component
+  </div>
+);
+
+Skills.propTypes = {};
+
+Skills.defaultProps = {};
+
+export default Skills;
